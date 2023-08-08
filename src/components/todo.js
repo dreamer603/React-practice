d636d1eaf6e0c2a4bb88fbd1e645d60a37bd73bf
dreamer603 <95961915+dreamer603@users.noreply.github.com>
2023-08-08 14:49:55 +0900

    Update todo.js

--- src/components/todo.js
+++ src/components/todo.js
@@ -2,9 +2,9 @@ function Todo(props) {
     return (
         <div className="todo" key={props.todo.id}> 
           <h3>
+            <label onClick={props.handleDelete}>&nbsp;&nbsp;&nbsp;❌ </label>
             <label className={props.todo.completed ? "completed" : null} onClick={props.handleClick}>
               {props.todo.todoName} </label>
-            <label onClick={props.handleDelete}>&nbsp;&nbsp;&nbsp;❌ </label>
           </h3>
         </div>
     )
